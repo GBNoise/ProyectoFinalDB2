d1357077aefc8256f13c276a337f4ed29d7e8653
GBNoise <gbolanos2002@gmail.com>
2022-07-14 01:25:40 -0600

    changed sizes of the window

--- frontend/src/main/main.ts
+++ frontend/src/main/main.ts
@@ -71,8 +71,10 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 1366,
+    height: 768,
+    minWidth: 1024,
+    minHeight: 728,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: app.isPackaged
