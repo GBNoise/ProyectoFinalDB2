904da33373ae5298ff164337f9d5a8b0beb8e996
GBNoise <gbolanos2002@gmail.com>
2022-07-16 04:37:22 -0600

    implemented the use of the productorRouter

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import { Application } from "express";
 import express from "express";
 import { router as userRouter } from "./Routers/usersRouter";
 import { router as linksRouter } from "./Routers/linksRouter";
+import { router as productorRouter } from "./Routers/productorRouter";
 import cors from "cors";
 import { config } from "dotenv";
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/links", linksRouter);
+app.use("/productor", productorRouter);
 
 const PORT = process.env.PORT || 8000;
 
