89447dde2689c90864df193f837cd19fdd182d46
GBNoise <gbolanos2002@gmail.com>
2022-07-16 04:35:29 -0600

    created productor service

---
+++ backend/src/Service/productor.service.ts
@@ -0,0 +1,32 @@
+import {
+  executeInsert,
+  executeSelect,
+  DBTables,
+  executeDeleteWhereID,
+  executeUpdateWhereID,
+} from "../database/dbconfig";
+import { Productor, ServerResponse } from "../Models/models";
+
+export const getAllProductores = (): Promise<ServerResponse> => {
+  return executeSelect({ from: DBTables.Productor });
+};
+
+export const getProductorByID = (id: number): Promise<ServerResponse> => {
+  return executeSelect({ from: DBTables.Productor, whereID: id });
+};
+
+export const createProductor = (
+  productor: Productor
+): Promise<ServerResponse> => {
+  return executeInsert(DBTables.Productor, productor);
+};
+
+export const deleteProductorWithID = (id: number): Promise<ServerResponse> => {
+  return executeDeleteWhereID(DBTables.Productor, id);
+};
+
+export const updateProductorWithID = (
+  productor: Productor
+): Promise<ServerResponse> => {
+  return executeUpdateWhereID(DBTables.Productor, productor);
+};
