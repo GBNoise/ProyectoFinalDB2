601cbf8b222221cefbaacbe89394bd246f67f665
GBNoise <gbolanos2002@gmail.com>
2022-07-22 05:56:18 -0600

    created tipo de riego

---
+++ frontend/src/renderer/components/TIpoDeRiego/TipoDeRiego.tsx
@@ -0,0 +1,52 @@
+import {
+  faTractor,
+  faUser,
+  faWheatAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { IconButtonProps, colors } from 'renderer/types/types';
+import { useState, useEffect, useContext } from 'react';
+import axios from '../../axios';
+import { TablePanel } from '../tablePanel';
+import { globalContext } from 'renderer/contexts/globalContext';
+
+const icons: IconButtonProps[] = [
+  { color: colors.pinkAccent, size: '2x', icon: faUser, to: '/productores' },
+  { color: colors.redAccent, size: '2x', icon: faTractor, to: '/finca' },
+  { color: colors.salmonAccent, size: '2x', icon: faWheatAlt, to: '/lote' },
+];
+
+export const TipoDeRiego = () => {
+  const { state, dispatch } = useContext(globalContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getData = async () => {
+    await axios
+      .get('/tipoderiego')
+      .then((res) => {
+        dispatch({ type: 'setCurrentData', payload: res.data });
+      })
+      .catch((e) => console.log(e));
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return !isLoading ? (
+    <TablePanel
+      references={icons}
+      data={state.currentData.object.rows}
+      columns={state.currentData.object.columns}
+      count={state.currentData.object.rows.length}
+      icon={faUser}
+      color={colors.pinkAccent}
+      title="Tipo de Riego"
+      endpoint="/tipoderiego"
+      getData={getData}
+    />
+  ) : (
+    <h1>Loading</h1>
+  );
+};
