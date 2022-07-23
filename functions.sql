6de0902b356801902fd3198930992f1e5f90f77f
GBNoise <gbolanos2002@gmail.com>
2022-07-23 08:23:47 -0600

    created some db functions

---
+++ functions.sql
@@ -0,0 +1,56 @@
+-- funcion para obtener el productor con mas fincas
+CREATE FUNCTION dbo.getProductorWithMostFincas()
+returns int
+as
+    begin
+        declare @id int
+
+        select top 1
+         @id=ProductorID
+        from Finca
+        GROUP BY ProductorID
+        ORDER BY count(*) DESC
+
+        return @id;
+    end
+go
+
+-- funcion para obtener la finca con mas lotes
+CREATE FUNCTION dbo.getFincaWithMostLotes()
+returns int
+as
+    begin
+        declare @id int
+
+        select top 1
+        @id = FincaID
+        from Lote
+        GROUP BY FincaID
+        order by count(*) DESC
+
+        return @id
+
+    end
+go
+
+-- funcion para obtener la bodega con mayor inventario de un producto especifico
+CREATE FUNCTION dbo.getBodegaIDWithMostOfCertainProduct(@productoID int)
+returns int
+as
+    begin
+        declare @id int
+
+        select TOP 1 @id=BodegaID from Inventario
+        WHERE ProductoID = @productoID
+        GROUP BY ProductoID, BodegaID
+        ORDER BY SUM(Cantidad) desc
+
+        return @id
+    end
+go
+
+
+
+
+
+
